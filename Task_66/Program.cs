f782a5512ff2541c4f8f974fcc921b0de8386dc0
Anna <ann1989@list.ru>
2022-08-26 02:41:18 +0300

    Added Task_66 + Task_68

---
+++ Task_66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int SumNumbers(int m, int n)
+{
+    if (m == n) return n;
+    else return m + SumNumbers(m += 1, n);
+}
+
+Console.WriteLine
+($"Сумма чисел в промежутке от {m} до {n} равна {SumNumbers(m, n)}");
---
+++ Task_68/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+int m = InputNumbers("Введите число m: ");
+int n = InputNumbers("Введите число n: ");
+int functionAkkerman = A(m, n);
+Console.Write($"-> A(m,n) = {functionAkkerman} ");
+int A(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else
+  if (n == 0) return A(m - 1, 1);
+  else
+return A(m - 1, A(m, n - 1));
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
